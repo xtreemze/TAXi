14b2888a9fd331be4b2cdbf83be0fb8629ac62ba
Carlos Velasco <carlos@2activedesign.com>
2016-05-12 02:02:02 +0200

    Update NPM

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  // "ecmaFeatures": {
+  // 	"jsx": true,
+  // 	"modules": true,
+  // 	"arrowFunctions": true,
+  // 	"classes": true,
+  // 	"spread": true,
+  // },
+  "env": {
+    "browser": true,
+    // "es6": true
+  },
+  "extends": "airbnb",
+  // "rules": {
+  //   	"indent": [ "error", "tab" ],
+  //   "linebreak-style": ["error", "windows"],
+  //    	"quotes": [ "error", "single" ],
+  //    	"semi": [ "error", "always" ]
+  // },
+  "globals": {
+    "$": true,
+    "Materialize": true,
+    "jQuery": true,
+    "intervalID": true
+  }
+};
